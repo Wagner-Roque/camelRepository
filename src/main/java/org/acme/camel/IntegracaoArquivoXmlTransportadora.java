bc94d431af09c61ea3319739920f30192b297acf
Wagner Roque <roquewagnertadeu@gmail.com>
2023-09-12 16:57:33 -0300

    Creating camel route for nfe xml

---
+++ src/main/java/org/acme/camel/IntegracaoArquivoXmlTransportadora.java
@@ -0,0 +1,34 @@
+package org.acme.camel;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.http.HttpConstants;
+import org.apache.camel.support.builder.Namespaces;
+
+public class IntegracaoArquivoXmlTransportadora extends RouteBuilder {
+    @Override
+    public void configure() throws Exception {
+        var ns = new Namespaces("ns","http://www.portalfiscal.inf.br/nfe");
+        from("file:{{diretorioEntrada}}?delay=5000")
+                .routeId("integracao-arquivo")
+                .log("Processando o arquivo: ${file:name}")
+                .setProperty("CNPJ",xpath("{{xpathCnpjTransportadora}}",ns))
+                .choice()
+                .when(exchangeProperty("CNPJ").isEqualTo("1"))
+                .to("file:{{diretorioSaida}}?fileName=${date:now:HHmmss}_${file:name}")
+                .when(exchangeProperty("CNPJ").isEqualTo("2"))
+                    .throttle(1).timePeriodMillis(5000).asyncDelayed()
+                        .setHeader(HttpConstants.HTTP_METHOD, constant("POST"))
+                        .setHeader(HttpConstants.HTTP_URI, constant("{{urlApiTransportadora2}}"))
+                        .setHeader(HttpConstants.HTTP_PATH, constant("nfes"))
+                        .setHeader(HttpConstants.CONTENT_TYPE, constant("application/xml"))
+                        .to("http:servidorTransportadora2")
+                .endChoice()
+        .otherwise()
+        .log("Transportadora não integrada")
+        .end();
+
+    }
+}
+
+
+
