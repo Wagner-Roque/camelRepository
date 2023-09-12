128ad3d21de2215027a47901fb5572104e75e13d
Wagner Roque <roquewagnertadeu@gmail.com>
2023-09-12 16:57:05 -0300

    Creating camel project example

---
+++ src/main/java/org/acme/camel/IntegracaoArquivo.java
@@ -0,0 +1,18 @@
+package org.acme.camel;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.file.FileConstants;
+
+public class IntegracaoArquivo extends RouteBuilder {
+    @Override
+    public void configure() throws Exception {
+        from("file:{{diretorioEntrada}}?delay=5000")
+                .routeId("integracao-arquivo")
+                .process(exchange -> {
+                    System.out.println(exchange.getMessage().getBody(String.class));
+                })
+                .log("Processando o arquivo: ${file:name}")
+                .setHeader(FileConstants.FILE_NAME,simple("${date:now:HHmmss}_${file:name}"))
+                .to("file:{{diretorioSaida}}");
+    }
+}
